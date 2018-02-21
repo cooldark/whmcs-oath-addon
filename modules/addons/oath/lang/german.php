096023eacbb33a5df181c63bb6f1097a2ea565b2
cooldark <bluetiger@freakmail.de>
2018-02-21 23:57:41 +0100

    Update german.php
    Fixed two typos, changed "Mobile + Mobilgerät" to "Smartphone", minor changes.

--- modules/addons/oath/lang/german.php
+++ modules/addons/oath/lang/german.php
@@ -1,27 +1,27 @@
 <?php
 
 $_ADDONLANG['incorrect']='Der eingegebene Code ist nicht korrekt.';
-$_ADDONLANG['enterCodeNote']='Bitte geben Sie den generierten Code aus Ihrer Mobile App ein.';
-$_ADDONLANG['backupCodeInfo']='Login via Sicherungscode Erfolgreich. Sicherungscodes sind nur einmal gültig. Es wird nun zurückgesetzt.';
-$_ADDONLANG['newBackupCodeNote']='Ihr neuer Sicherungscode ist';
+$_ADDONLANG['enterCodeNote']='Bitte geben Sie den generierten Code aus der App Ihres Smartphones ein.';
+$_ADDONLANG['backupCodeInfo']='Login via Sicherungscode erfolgreich. Sicherungscodes sind nur einmal gültig. Es wird nun zurückgesetzt.';
+$_ADDONLANG['newBackupCodeNote']='Ihr neuer Sicherungscode lautet:';
 $_ADDONLANG['enterCode']='Ihren Code eingeben';
 $_ADDONLANG['btnLogin']='Login validieren';
 $_ADDONLANG['enterEmCode']='Ihren Notfall-Code eingeben';
 $_ADDONLANG['emLogin']='Notfall Login';
-$_ADDONLANG['lostDevice']='Haben Sie Ihr Mobilgerät verloren?';
+$_ADDONLANG['lostDevice']='Haben Sie Ihr Smartphone verloren?';
 $_ADDONLANG['inactive']='Unterstützung der Zwei-Faktor-Authentifizierung ist zurzeit deaktiviert.';
 $_ADDONLANG['disabled']='Zwei-Faktor-Authentifizierung ist deaktiviert.';
 $_ADDONLANG['btnEnable']='Zwei-Faktor-Authentifizierung aktivieren';
-$_ADDONLANG['scanNote']='Bitte scannen Sie diesen QR Code mit Ihrer Mobile Authentifizierungs App.';
+$_ADDONLANG['scanNote']='Bitte scannen Sie diesen QR-Code mit Ihrer Authentifizierungs-App.';
 $_ADDONLANG['unableScan']='Falls scannen nicht möglich ist, benutzen Sie diese Geheimziffern:';
 $_ADDONLANG['verify']='Code Verifizierung';
-$_ADDONLANG['recomApp']='Empfohlene Mobile App';
+$_ADDONLANG['recomApp']='Empfohlene Smartphone-App';
 $_ADDONLANG['enabled']='Zwei-Faktor-Authentifizierung ist aktiv.';
-$_ADDONLANG['emCode']='Ihren Notfall-Code lautet';
-$_ADDONLANG['emCodeNote']='Sie können diesen verwenden, falls Sie Ihr Mobilgerät verlieren.';
+$_ADDONLANG['emCode']='Ihr Notfall-Code lautet';
+$_ADDONLANG['emCodeNote']='Sie können diesen verwenden, falls Sie Ihr Smartphone verlieren.';
 $_ADDONLANG['btnSecret']='Geheimziffern ansehen';
-$_ADDONLANG['emCodeNote2']='Notieren Sie diesen, dieser wird Ihnen nicht mehr wieder angezeigt.';
+$_ADDONLANG['emCodeNote2']='Notieren Sie diesen, dieser kann Ihnen nicht erneut angezeigt werden.';
 $_ADDONLANG['btnDisable']='Zwei-Faktor-Authentifizierung deaktivieren';
-$_ADDONLANG['note']='Hinweis: Alle Unterbenutzer mit Zugriff auf Ihren Account müssen die Zwei-Faktor-Authentifizierung ebenfalls angeben.';
+$_ADDONLANG['note']='Hinweis: Alle weiteren Benutzer mit Zugriff auf Ihren Account müssen die Zwei-Faktor-Authentifizierung ebenfalls angeben.';
 
 ?>
